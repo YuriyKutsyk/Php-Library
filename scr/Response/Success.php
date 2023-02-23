4a7d8f4ca5eedc24b01e3710b144a92fa1abe600
Kutsyk <rura4576@uk.net>
2023-02-23 14:56:29 +0200

    Add some changes

---
+++ scr/Response/Success.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Scr\Response;
+
+class Success extends Response
+{
+    private array $data;
+
+    public function __construct($response)
+    {
+        if (!empty($response['message'])) {
+            parent::__construct($response['message']);
+        }
+        if (!empty($response['data'])) {
+            $this->data = $response['data'];
+        }
+    }
+
+    public function getData(): mixed
+    {
+        return $this->data;
+    }
+
+    public function hasData(): bool
+    {
+        return $this->data !== null;
+    }
+}
